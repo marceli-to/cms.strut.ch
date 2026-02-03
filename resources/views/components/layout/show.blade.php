1961c7553e2cd9daf473cc41b7ee2f915ced4ddf
Marcel Stadelmann <m@marceli.to>
2026-02-03 15:00:48 +0100

    wip

--- resources/views/components/layout/show.blade.php
+++ resources/views/components/layout/show.blade.php
@@ -2,15 +2,23 @@
   'title' => null,
   'location' => null
 ])
+@php
+  $referer = request()->headers->get('referer', '');
+  $refererHost = parse_url($referer, PHP_URL_HOST);
+  $isInternalReferrer = $refererHost === request()->getHost();
+@endphp
 <x-layout.partials.head />
 
 <x-layout.partials.body>
 
   <x-layout.partials.header class="flex gap-x-30 md:gap-x-0 md:grid md:grid-cols-12 w-full pt-20 lg:pt-40 px-20 md:px-0 mb-20 lg:mb-40">
-    
+
     <div class="md:col-span-3 md:pl-20 lg:pl-40">
 
-      <a href="{{ route('page.works') }}" class="block mt-7">
+      <a
+        href="{{ route('page.works') }}"
+        @if($isInternalReferrer) onclick="history.back(); return false;" @endif
+        class="block mt-7">
         <x-icons.arrow-left size="lg" class="w-20 h-auto md:w-43" />
       </a>
 
